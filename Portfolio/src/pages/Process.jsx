845f7b4346472bbe565d9f2d33c0818b68b40237
Jonah, KemfonAbasi Mfon <kemfonabasijonah@gmail.com>
2026-06-11 11:04:06 +0100

    your message

--- Portfolio/src/pages/Process.jsx
+++ Portfolio/src/pages/Process.jsx
@@ -1,7 +1,7 @@
 function ProcessSection() {
   return(
     <>
-      <section className="w-full pb-20 flex justify-center items-center bg-[#0a0a0b]">
+      <section className="w-full pb-[25px] flex justify-center items-center bg-[#0a0a0b]">
         <div className="w-[90%] flex flex-col">
           <p className="font-semibold text-[1.9rem] tracking-0.5 text-[#E5E5E5]">
             Turning imagination into<br/><span className="text-[#515151]">responsive, modern, and impactful web experiences.</span>
